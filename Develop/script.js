42213884eaf7d07209109fef027732e0374ffbe5
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-04-20 01:55:51 +0300

    updated id for popup and prompt in html

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,14 @@
 // Assignment code here generate password
+
 function generatePassword() {
-  return "WILL GENERATE PASSWORD";  
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
 }
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
