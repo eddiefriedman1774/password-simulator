d65119bb911a81afa70cadac080903d978088912
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-04-20 02:18:32 +0300

    add buttons

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,17 @@
 function generatePassword() {
   let popup = document.getElementById("popup");
   popup.style.visibility = "visible";
+
 }
+function submitform(){
+  let numberinput = document.getElementById("number");
+  let caplettersinput = document.getElementById("capletters");
+  let lowlettersinput = document.getElementById("lowletters");
+  let numlettersinput = document.getElementById("numletters");
+  let speciallettersinput = document.getElementById("specialletters");
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
